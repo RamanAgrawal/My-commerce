19515cea843c1e3b2c643bc2d241b82eb649adde
Raman Agrawal <iramanagrawal@gmail.com>
2023-10-14 20:45:10 +0530

    AddAddress Page

---
+++ src/pages/AddAddress.tsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import AddAddressForm from "../components/AddAddressForm"
+import { AddressI } from "../models/Models";
+import { selectLoggedInuser, updateUserAsync } from "../components/auth/authSlice";
+import { AppDispatch } from "../store/store";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+const AddAddress = () => {
+    const user = useSelector(selectLoggedInuser);
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate=useNavigate()
+    // React Hook Form setup for form handling
+    const { reset } = useForm<AddressI>();
+    const handleSubmitHandler = (address: AddressI) => {
+        if (user) {
+            dispatch(
+                updateUserAsync({ ...user, addresses: [...user.addresses, address] })
+            );
+        }
+        reset();
+        navigate('/profile')
+       
+    }
+    return (
+        <div className="mx-auto max-w-xl px-2 sm:px-6 lg:px-8 bg-white pt-4">
+            <AddAddressForm handleSubmitHandler={handleSubmitHandler} />
+        </div>
+    )
+}
+
+export default AddAddress
